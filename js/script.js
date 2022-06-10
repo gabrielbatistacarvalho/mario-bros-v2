1c4f7bcb3b0ea23acc72c7a175ca912bf03fe5ad
gabrielbatistacarvalho <gabriel_carvalho26@hotmail.com>
2022-06-10 08:39:27 -0300

    Função jump só é acionada quando pressionado espaço e músicas adicionadas

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-const mario = document.querySelector('.mario');
-const pipe = document.querySelector('.pipe');
+const mario  = document.querySelector('.mario');
+const pipe   = document.querySelector('.pipe');
+const clouds = document.querySelector('.clouds');
 
 const jump = () => {
 
@@ -13,24 +14,37 @@ const jump = () => {
 
 const loop = setInterval(() => {
 
-    const pipePosition = pipe.offsetLeft; 
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const marioPosition  = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const pipePosition   = pipe.offsetLeft; 
+    const cloudsPosition = clouds.offsetLeft; 
     
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`;
 
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        clouds.style.animation = 'none';
+        clouds.style.left = `${cloudsPosition}px`;
+
         mario.src = './images/game-over.png';
         mario.style.width = '75px';
         mario.style.marginLeft = '50px';
+
+        document.getElementById('txtRestart').style.display = 'block';
+        document.getElementById('txtRestart').focus();
 
         clearInterval(loop);
     }
 
 }, 10);
 
-document.addEventListener('keydown', jump);
+document.body.addEventListener('keypress', () =>{
+    var x = event.keyCode;
+    if(x == '0032')
+    {
+        jump();
+    }
+});
